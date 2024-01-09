47764a70d5a500a63add307dd661dea55751ec4f
Loreperni <Lorenzo.perniciano@bywaves.it>
2024-01-09 19:38:18 +0100

    prove contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
   { path: 'homepage', component: HomepageComponent },
   { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
   declarations: [
@@ -15,8 +17,9 @@ import { FooterComponent } from './footer/footer.component';
     HomepageComponent,
     NavBarComponent,
     FooterComponent,
+    ContactComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
